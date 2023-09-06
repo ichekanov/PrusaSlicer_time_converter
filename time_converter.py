ec05429b8bc676964d5402468bc99dcb44885cd1
Ivan Chekanov <78589729+ichekanov@users.noreply.github.com>
2023-09-06 14:30:50 +0300

    :sparkles: Add support for all possible M73 formats

--- time_converter.py
+++ time_converter.py
@@ -1,17 +1,33 @@
 import sys
 
+
+def get_time(minutes: int) -> str:
+    return f"{str(minutes//60).zfill(2)}:{str(minutes%60).zfill(2)}"
+
+
+def get_progress(percents: int) -> str:
+    return f"{str(percents).rjust(2)}%"
+
+
 filename = sys.argv[1]
 
-with open(filename, 'r', encoding='utf-8') as file:
+with open(filename, "r", encoding="utf-8") as file:
     data = file.readlines()
 
-with open(filename, 'w', encoding='utf-8') as file:
+with open(filename, "w", encoding="utf-8") as file:
     for row in data:
-        if 'M73' not in row:
+        if not row.startswith("M73"):
             file.write(row)
             continue
-        progress, minutes = [int(m[1:]) for m in row.split()[1:]] # percents and remaining time in minutes
-        new_row = 'M117 '
-        new_row += f'{str(minutes//60).zfill(2)}:{str(minutes%60).zfill(2)} left'
-        new_row += f' / {str(progress).rjust(2)}% done\n'
+        # https://reprap.org/wiki/G-code#M73:_Set.2FGet_build_percentage
+        row = row.replace("D", "C").replace("S", "R").replace("Q", "P")
+        args = {m[0]: int(m[1:]) for m in row.split()[1:]}  #  Get command arguments
+        if "C" in args:
+            #  Time to change/pause/user interaction
+            minutes = args["C"]
+            new_row = f"M117 {get_time(minutes)} to pause\n"
+        else:
+            #  Time until printing is done and progress done
+            progress, minutes = args["P"], args["R"]
+            new_row = f"M117 {get_time(minutes)} left / {get_progress(progress)} done\n"
         file.write(new_row)
